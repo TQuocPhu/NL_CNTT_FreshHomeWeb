4b6850c0a31cba890888b8ab4208f580e649f9a4
TQuocPhu <tqphu240804@gmail.com>
2026-01-28 20:57:55 +0700

    Chức năng: Khách hàng hủy đơn hàng khi đơn đang ở trạng thái chờ xác nhận

--- app/Http/Controllers/Clients/OrderController.php
+++ app/Http/Controllers/Clients/OrderController.php
@@ -3,16 +3,71 @@
 namespace App\Http\Controllers\Clients;
 
 use App\Http\Controllers\Controller;
+use App\Models\Coupon;
 use App\Models\Order;
+use App\Models\OrderStatusHistory;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+
+use function Flasher\Toastr\Prime\toastr;
 
 class OrderController extends Controller
 {
-    public function showOrderDetail($id) {
+    public function showOrderDetail($id)
+    {
         $order = Order::with(['orderItems.product', 'user', 'shippingAddress', 'payment', 'coupon'])->findOrFail($id);
         $user = Auth::user();
-        
+
         return view('clients.pages.order-detail', compact('order'));
+    }
+
+    public function canceledOrder($id)
+    {
+        DB::beginTransaction();
+
+        try {
+            $user = Auth::user();
+            $order = Order::where('id', $id)->where('user_id', $user->id)->with('orderItems.product')->firstOrFail();
+
+            // Hoàn kho
+            foreach ($order->orderItems as $item) {
+                $item->product->increment('stock', $item->quantity);
+            }
+
+            //Hoàn coupon (nếu có)
+            if ($order->coupon_id) {
+                Coupon::where('id', $order->coupon_id)
+                    ->where('used_count', '>', 0)
+                    ->decrement('used_count');
+            }
+
+            //Cập nhật trạng thái đơn hàng
+            $order->update(['status' => 'canceled']);
+
+            //update payment
+            if ($order->payment) {
+                $order->payment->update([
+                    'status' => 'failed'
+                ]);
+            }
+
+            //Lưu lịch sử trạng thái
+            OrderStatusHistory::create([
+                'order_id' => $order->id,
+                'status' => 'canceled',
+                'changed_at' => now(),
+                'note' => 'Khách hàng hủy đơn hàng'
+            ]);
+
+            DB::commit();
+
+            toastr()->success('Đơn hàng đã được hủy thành công và sản phẩm được hoàn kho');
+            return redirect()->back();
+        } catch (\Exception $e) {
+            DB::rollBack();
+            toastr()->error('Không thể hủy đơn hàng. Vui lòng thử lại. Lỗi: ' . $e);
+            return redirect()->back();
+        }
     }
 }
