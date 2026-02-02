ae3b6e5e067668b608f1f48392d289ecc04a2637
TQuocPhu <tqphu240804@gmail.com>
2026-02-02 22:28:56 +0700

    Thêm middleware kiểm tra đăng nhập admin, redirect về trang login nếu chưa đăng nhập

--- routes/admin.php
+++ routes/admin.php
@@ -1,15 +1,20 @@
 <?php
 
 use App\Http\Controllers\Admin\AdminAuthController;
+use App\Http\Controllers\Admin\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('admin')->group(function () {
 
-    Route::get('/dashboard', function() {
-        return view('admin.pages.dashboard');
-    })->name('admin.dashboard');
 
-    Route::get('/login', [AdminAuthController::class, 'showLoginForm'])->name('admin.login');
-    Route::post('/login', [AdminAuthController::class, 'loginAdmin'])->name('admin.login-post');
-    Route::get('/logout', [AdminAuthController::class, 'logout'])->name('admin.logout');
+
+    Route::middleware(['check.auth.admin'])->group(function () {
+        Route::get('/login', [AdminAuthController::class, 'showLoginForm'])->name('admin.login');
+        Route::post('/login', [AdminAuthController::class, 'loginAdmin'])->name('admin.login-post');
+    });
+
+    Route::middleware(['auth.custom'])->group(function () {
+        Route::get('/dashboard', [DashboardController::class, 'index'])->name('admin.dashboard');
+        Route::get('/logout', [AdminAuthController::class, 'logout'])->name('admin.logout');
+    });
 });
